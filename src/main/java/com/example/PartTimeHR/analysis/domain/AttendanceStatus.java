7f105beb709289f6ddcb376c003cc861feb2ff20
HyunDong <dh655933@gmail.com>
2026-01-24 13:50:56 +0900

    rollback Feature

--- src/main/java/com/example/PartTimeHR/analysis/domain/AttendanceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.PartTimeHR.analysis.domain;
-
-public enum AttendanceStatus {
-    NORMAL,   // 정상 출근
-    ABSENT,   // 결근
-    LATE      // 지각
-}
--- src/main/java/com/example/PartTimeHR/analysis/domain/WorkFlag.java
+++
@@ -1,7 +0,0 @@
-package com.example.PartTimeHR.analysis.domain;
-
-public enum WorkFlag {
-    OVERTIME,      // 연장 근무
-    HOLIDAY_WORK,  // 휴일 근무 (나중에)
-    NIGHT_SHIFT    // 야간 근무 (나중에)
-}
